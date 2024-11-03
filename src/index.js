3fcee8621d36e9399129fc921670c2b6b5d7d388
Paras Bagri <parasba85@gmail.com>
2024-11-04 01:33:50 +0530

    initilized client, apoloclient, inMemoeyCache,

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import { UserProvider } from "./contexts/user.context";
+import { CategoriesProvider } from "./contexts/categories.context";
+import { CartProvider } from "./contexts/cart.context";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+import "./index.scss";
+
+// initilizing the client interface
+
+const client = new ApolloClient({
+  uri: "https://crwn-clothing.com/",
+  cache: new InMemoryCache(),
+});
+
+const rootElement = document.getElementById("root");
+
+render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>,
+  rootElement
+);

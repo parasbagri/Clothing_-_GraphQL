6a729730e597b79388a4722c88aa89a627334b30
Paras Bagri <parasba85@gmail.com>
2024-11-04 02:38:06 +0530

    getting data in console , now going to use that

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,22 +1,41 @@
 import { createContext, useState, useEffect } from 'react';
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+// import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { gql,useQuery } from '@apollo/client'; 
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
-});
+}); 
+const COLLECTIONS = gql`
+query{
+collections{
+   id
+   title
+   items{
+      id
+      name
+      price
+      imageUrl
+   }
+}
+}`  
+// ab jo query banaye ho usko use karna hoga inside the provider
 
 export const CategoriesProvider = ({ children }) => {
+  const {loading, error, data} = useQuery(COLLECTIONS) // it is hook it eturns response
+  console.log("loading:", loading); 
+  console.log(data);   // ab hume data mil rha hai ab isko hum use karenge 
+
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+  // useEffect(() => {
+  //   const getCategoriesMap = async () => {
+  //     const categoryMap = await getCategoriesAndDocuments();
+  //     setCategoriesMap(categoryMap);
+  //   };
 
-    getCategoriesMap();
-  }, []);
+  //   getCategoriesMap();
+  // }, []);
 
   const value = { categoriesMap };
   return (
